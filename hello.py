05ed008242fa9134049364242e9f1ff62763943c
jacunar <josueally@gmail.com>
2023-03-03 11:27:33 -0600

    Pruebas de python - ejecución en la terminal

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print("Hola Mundo...")
+print("Mi nombre es Josué Acuña")
+
+msg = "Soy ingeniero en sistemas"
+msg2 = msg.capitalize()
+
+print(msg2)
+# Para ejecutar el script de python, hazlo de la siguiente forma, usando la terminal
+# Ruta de ubicación del ejecutable python Ruta del script de python
+# C:/Users/jacuna/AppData/Local/Programs/Python/Python311/python.exe g:/DATOS/_ALLY/PythonEx/hello.py
